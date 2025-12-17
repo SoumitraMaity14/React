4a3e60b195a771694bd3c0254bd4e64d524663fc
SoumitraMaity14 <maitychottu03225@gmail.com>
2025-12-17 11:43:00 +0530

    Refactor CounterReducer with action handling

--- practice/src/components/reducer/CounterReducer.tsx
+++ practice/src/components/reducer/CounterReducer.tsx
@@ -1,14 +1,28 @@
 import { useReducer } from "react"
-const counterInitialstate=0
-const counterReducer=(state, action)=>{
-    
+
+const initialState = 0
+const counterReducer = (state: number, action:string) => {
+    switch (action) {
+        case "Increment":
+            return state + 1
+        case "Decrement":
+            return state - 1
+        case "Reset":
+            return initialState
+        default:
+            return state
+    }
 }
 
 export const CounterReducer = () => {
-    const [count, dispatch]=useReducer(counterReducer, counterInitialstate)
-  return (
-    <div>
-
-    </div>
-  )
+    const [count, dispatch]=useReducer(counterReducer, initialState)
+    return (
+        <div>
+            
+            <h2>counter:{count}</h2>
+        <button onClick={()=>dispatch("Increment")}>Increment</button>
+        <button onClick={()=>dispatch("Decrement")}>Decrement</button>
+        <button onClick={()=>dispatch("Reset")}>Reset</button>
+        </div>
+    )
 }
