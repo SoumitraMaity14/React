65a750a64cd30529f2464811e0c86f943b903466
SOumitra Maity <maitychottu03225@gmail.com>
2025-12-21 23:26:17 +0530

    new file added

--- practice/src/components/reducer/ShoppingCartWithReducer.tsx
+++ practice/src/components/reducer/ShoppingCartWithReducer.tsx
@@ -44,6 +44,42 @@ const reducer = (state, action) => {
                 )
             }
         }
+        case "REMOVE_ITEMS":{
+            const filteredItems=state.items.filter((item)=> item.id !== action.payload.id)
+            return {
+                ...state,
+                items: filteredItems,
+                totalAmount:filteredItems.reduce(
+                    (total, item)=> total + item.price * item.quantity,  0
+                ),
+                totalItems: filteredItems.reduce(
+                    (total, item)=> total + item.quantity, 0
+                )
+            }
+
+        }
+        case "UPDATE_ITEMS":{
+            if(action.payload.quantity===0){
+                return reducer(state, {
+                    type: "REMOVE_ITEMS",
+                    payload: {id:action.payload.id}
+                })
+            }
+            const updateQuantityItems=state.items.map((item)=>
+            item.id===action.payload.id ?{...item, quantity: action.payload.quantity}: item)
+            return {
+                ...state,
+                items: updateQuantityItems,
+                totalAmount: updateQuantityItems.reduce(
+                    (total, item)=> total +item.price * item.quantity, 0
+                ),
+                totalItems: updateQuantityItems.reduce((total, item)=> total+item .quantity, 0) 
+            }
+        }
+        case "CLEAR_CART":
+            return initialState;
+        default:
+            return state
 
     }
 }
@@ -75,7 +111,7 @@ export const ShoppingCartWithReducer = () => {
             <div>
                 <h2>Shopping Cart</h2>
                 {state.items.length === 0 ? (
-                    <div>Your car is empty
+                    <div>Your cart is empty
                     </div>
                 ) : (
                     <div>
@@ -84,12 +120,28 @@ export const ShoppingCartWithReducer = () => {
                                 <p>
                                     {item.name}-{item.price}*{item.quantity}
                                 </p>
+                                 <button onClick={()=> dispatch({
+                                    type: "UPDATE_ITEMS",
+                                    payload: {id:item.id, quantity: item.quantity-1}
+                                 })}>-</button>
+                                  <button onClick={()=> dispatch({
+                                    type: "UPDATE_ITEMS",
+                                    payload: {id:item.id, quantity: item.quantity+1}
+                                 })}>+</button>
+                                <button onClick={()=>dispatch({
+                                    type: "REMOVE_ITEMS",
+                                    payload: {id: item.id}
+
+                                })}>Remove</button>
                             </div>
                         ))}
                     </div>
                 )}
                 <h3>Total Items: {state.totalItems}</h3>
                 <h3>Total Price: {state.totalAmount} </h3>
+                {state.items.length>0 &&(
+                    <button onClick={()=>dispatch({type: "CLEAR_CART"})}>CLEAR CART</button>
+                )}
             </div>
 
         </div>
