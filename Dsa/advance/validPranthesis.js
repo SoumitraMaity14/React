abb43363e60f1c0978e2b426c078a12be4c77c9e
SOumitra Maity <maitychottu03225@gmail.com>
2026-02-17 23:51:25 +0530

    valid pranthesis

---
+++ Dsa/advance/validPranthesis.js
@@ -0,0 +1,20 @@
+function validPranthesis(s){
+    let stack=[]
+    let map={
+        ")":"(",
+        "}": "{",
+        "]":"["
+    }
+    for(let char of s){
+        if(char==="("||char==="{"||char==="["){
+            stack.push(char)
+        }
+        else{
+            if(stack.length===0 || stack.pop() !==map[char]){
+                return false
+            }
+        }
+    }
+    return stack.length===0
+}
+console.log(validPranthesis("({})"))
