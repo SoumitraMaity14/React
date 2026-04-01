46aaf6f3b60c8ae8c6666cd9b8fef54ba0cc1bf7
SOumitra Maity <maitychottu03225@gmail.com>
2026-04-01 23:58:43 +0530

    restart begin

---
+++ Dsa/weekly-practice/restart.py
@@ -0,0 +1,11 @@
+def palindrome(s):
+    left=0
+    right=len(s)-1
+    while left<right:
+        if s[left]!=s[right]:
+            return False
+        left+=1
+        right-=1
+    return True
+
+print(palindrome([1,2,1,2]))
