14cf04ff9a4bb7256f7f5e80240bfc1d6fac88d4
SOumitra Maity <maitychottu03225@gmail.com>
2026-04-23 23:35:30 +0530

    mirror distance of integer is added

--- Dsa/common/targetArray.py
+++ Dsa/common/targetArray.py
@@ -5,3 +5,16 @@ def targetArray(nums, index):
         target.insert(index[i],nums[i])
     return target
 print(targetArray([0,1,2,3,4],[0,1,2,2,1]))
+
+def minimum_distance(n):
+    # s=str(n)
+    # left=0
+    # right=len(s)-1
+    # mirror=0
+    # while left<right:
+    #     mirror+=abs(int(s[left])-int(s[right]))
+    #     left+=1
+    #     right-=1
+    # return mirror
+    return abs(n- int(str(n)[::-1]))
+print(minimum_distance(25))
