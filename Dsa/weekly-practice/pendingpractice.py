8b27228874e00c2e70f177d5d6321efddad9e84d
SOumitra Maity <maitychottu03225@gmail.com>
2026-05-03 23:49:20 +0530

    CHange postion practice done

--- Dsa/weekly-practice/pendingpractice.py
+++ Dsa/weekly-practice/pendingpractice.py
@@ -50,3 +50,17 @@ def change_postion(arr):
         ans[i]=arr[arr[i]]
     return ans
 print(change_postion([0,3,2,4,1,5,6]))
+
+def perfectNumber(num):
+    if num<=1:
+        return False
+    def_total=1
+    for i in range(2, int(num**0.5)+1):
+        if num%i==0:
+            def_total+=i
+            if i != num//i:
+                def_total+=num//i
+    return def_total==num
+print(perfectNumber(28))
+
+
