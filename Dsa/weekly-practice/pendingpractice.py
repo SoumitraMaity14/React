78fda73fcf9d421f2da20be04904fa481b169dad
SOumitra Maity <maitychottu03225@gmail.com>
2026-05-05 23:08:55 +0530

    sumTarget are added

--- Dsa/weekly-practice/pendingpractice.py
+++ Dsa/weekly-practice/pendingpractice.py
@@ -73,3 +73,12 @@ def findFriendsOrder(order, friends):
 print(findFriendsOrder([1,2,4,5,7],[2,7,5]))
 
 
+def sumTarget(arr, target):
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            if arr[i]+arr[j]==target:
+                return [i,j]
+print(sumTarget([2,3,4,5,2,6],9))
+
+
+
