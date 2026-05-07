d25e22059736acdf05d940fa89f3331c5b3c700c
SOumitra Maity <maitychottu03225@gmail.com>
2026-05-07 22:23:43 +0530

    update file fast api added

--- Dsa/weekly-practice/api-practice.py
+++ Dsa/weekly-practice/api-practice.py
@@ -1,3 +1,9 @@
+from fastapi import fastAPI, Depends
+
+from sqlalchemy.orm import Session
+
+
+
 def get_db():
     db=session()
     try:
@@ -17,3 +23,18 @@ def create_product(product:Product, db:Session=Depends(get_db)):
 def get_product(db:Session=Depends(get_db)):
     db_product=db.query(database_model.Product).all
     return db_product
+
+@app.put("/product")
+
+def update_product(id:int, product:Product, db=Session=Depends(get_db)):
+    db_product=db.query(database_model.Product).filter(database_model.Product.id==id).first()
+    if db_product:
+        db_product.name=product.name
+        db_product.description=product.description
+        db_product.price=product.price
+        db_product.quantity=product.price
+        db.commit()
+        return "product updated"
+    else:
+        return "product not updated"
+    
