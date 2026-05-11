2b079dd660577c64b5e94232636c5b92304c114f
SOumitra Maity <maitychottu03225@gmail.com>
2026-05-11 23:56:02 +0530

    update code

--- Dsa/weekly-practice/countLengthIndex.py
+++ Dsa/weekly-practice/countLengthIndex.py
@@ -7,3 +7,16 @@ def countLengthIndex(n):
         count+=1
     return count, index
 print(countLengthIndex([2,2,3,4,4,5,6,7,8]))
+
+def arraySort(arr):
+    n=len(arr)
+    for i in range(n):
+        for j in range(n-1-i):
+            if arr[j]<arr[j+1]:
+                arr[j], arr[j+1]=arr[j+1], arr[j]
+    return arr
+print(arraySort([2,3,4,5]))
+
+def aimplesort(arr):
+    return arr[::-1]
+print(aimplesort([2,3,4,5]))
